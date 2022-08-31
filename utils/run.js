aa225a822a3c51ca93c5d1cb7edacc5719296b8a
fancn21th <fancn21th@gmail.com>
2022-08-31 13:32:08 +0800

    错误处理

--- utils/run.js
+++ utils/run.js
@@ -1,4 +1,4 @@
-const { dim, italic, bold, blue, bgBlue } = require('chalk');
+const { dim, italic, bold, bgRed, blue, bgBlue, bgGreen } = require('chalk');
 const ora = require('ora');
 const fs = require('fs');
 const path = require('path');
@@ -9,6 +9,7 @@ module.exports = async function (flags) {
 		text: '转换准备开始'
 	});
 
+	console.log();
 	console.log(
 		blue(
 			`支持以下 Case Style
@@ -16,6 +17,7 @@ camelCase[testString], constantCase[TEST_STRING], dotCase[test.string], paramCas
 		)
 	);
 
+	console.log();
 	console.log(bgBlue(`当前配置是 -s=${flags.style}`));
 
 	spinner.start();
@@ -23,25 +25,32 @@ camelCase[testString], constantCase[TEST_STRING], dotCase[test.string], paramCas
 	try {
 		const filePath = path.join(process.cwd(), 'input.json');
 
-		fs.readFile(filePath, 'utf8', function (err, data) {
-			if (err) throw err;
+		if (fs.existsSync(filePath)) {
+			fs.readFile(filePath, 'utf8', function (err, data) {
+				if (err) throw err;
 
-			const output = p2map(JSON.parse(data), {
-				style: flags.style,
-				suffix: flags.suffix,
-				prefix: flags.prefix
+				const output = p2map(JSON.parse(data), {
+					style: flags.style,
+					suffix: flags.suffix,
+					prefix: flags.prefix
+				});
+
+				if (flags.dryRun) {
+					console.log(output);
+				}
+
+				console.log();
+				console.log(console.log('转换成功'));
 			});
-
-			if (flags.dryRun) {
-				console.log(output);
-			}
-		});
+		} else {
+			throw new Error(
+				`${process.cwd()} 目录下没有找到 "input.json" 文件 请创建`
+			);
+		}
 	} catch (error) {
-		spinner.fail(error.message);
+		spinner.fail(bgRed(error.message));
 		process.exit(1);
 	} finally {
 		spinner.stop();
-		console.log();
-		console.log('转换成功');
 	}
 };
